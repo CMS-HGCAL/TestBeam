b4d8e0eadffdc8692d4cfbb3edeefcfdb9e73213
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-02-25 19:52:04 +0100

    updated documentation and reformat of code

--- Geometry/interface/HGCalTBTopology.h
+++ Geometry/interface/HGCalTBTopology.h
@@ -2,17 +2,18 @@
 #define HGCAL_GEOMETRY_HGCALTBTOPOLOGY_H 1
 
 /** \class HGCalTBTopology
-  *  
+  *
   * Reference: https://indico.cern.ch/event/456955/
-  * 
+  *
   * $Date: $
   * $Revision: $
   * \author J. Mans - Minnesota
   */
-class HGCalTBTopology {
+class HGCalTBTopology
+{
 public:
-  // valid sensorSizes are 128 and 256
-  bool ix_iv_valid(int ix, int iv, int sensorSize) const;
+	// valid sensorSizes are 128 and 256
+	bool ix_iv_valid(int ix, int iv, int sensorSize) const;
 };
 
 
