815973f7f047c9f0b0d670487a3bdac14e0aa5e5
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-02-29 15:12:35 +0100

    documentation and moved electronic map

--- RawToDigi/test/testRawToDigi_cfg.py
+++ RawToDigi/test/testRawToDigi_cfg.py
@@ -4,8 +4,8 @@ process = cms.Process("unpack")
 
 
 process.source = cms.Source("HGCalTBTextSource",
-                            run=cms.untracked.int32(2), ###! maybe this should be read from the file
-                            fileNames=cms.untracked.vstring("file:SKIROC_RO.txt")
+                            run=cms.untracked.int32(2), ### maybe this should be read from the file
+                            fileNames=cms.untracked.vstring("file:SKIROC_RO.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
 
 )
 
@@ -15,9 +15,11 @@ process.dumpRaw = cms.EDAnalyzer("DumpFEDRawDataProduct",
 process.dumpDigi = cms.EDAnalyzer("HGCalDigiDump")
 
 process.hgcaldigis = cms.EDProducer("HGCalTBRawToDigi",
-                                    InputLabel=cms.InputTag("source"),
-                                    fedId=cms.untracked.int32(1000),
-                                    electronicsMap=cms.untracked.string("HGCal/CondObjects/test/map_FNAL.txt")
+                                    InputLabel=cms.InputTag("source"), ###
+                                    fedId=cms.untracked.int32(1000), ## FED ID is hard coded in the HGCalTBTextSource TO BE FIXED
+
+                                    electronicsMap=cms.untracked.string("HGCal/CondObjects/data/map_FNAL.txt"), ### electronicsMap written by hand
+
                                     )
 
 process.p =cms.Path(process.dumpRaw*process.hgcaldigis*process.dumpDigi)
