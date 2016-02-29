99b61380bb957de04c1e8e2e2a7c0ac1b7cb1be8
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-02-29 16:26:57 +0100

    Reformatted

--- DataFormats/plugins/HGCalDigiDump.cc
+++ DataFormats/plugins/HGCalDigiDump.cc
@@ -8,37 +8,40 @@
 using namespace std;
 
 /** \class HGCalDigiDump
-      
+
 \author J. Mans - Minnesota
 */
-class HGCalDigiDump : public edm::EDAnalyzer {
+class HGCalDigiDump : public edm::EDAnalyzer
+{
 public:
-  explicit HGCalDigiDump(edm::ParameterSet const& conf);
-  virtual void analyze(edm::Event const& e, edm::EventSetup const& c);
+	explicit HGCalDigiDump(edm::ParameterSet const& conf);
+	virtual void analyze(edm::Event const& e, edm::EventSetup const& c);
 };
 
 
-HGCalDigiDump::HGCalDigiDump(edm::ParameterSet const& conf) {
-  consumesMany<SKIROC2DigiCollection>();
+HGCalDigiDump::HGCalDigiDump(edm::ParameterSet const& conf)
+{
+	consumesMany<SKIROC2DigiCollection>();
 }
-void HGCalDigiDump::analyze(edm::Event const& e, edm::EventSetup const& c) {
+void HGCalDigiDump::analyze(edm::Event const& e, edm::EventSetup const& c)
+{
 
-  std::vector<edm::Handle<SKIROC2DigiCollection> > ski;
+	std::vector<edm::Handle<SKIROC2DigiCollection> > ski;
 
-  try {
-    e.getManyByType(ski);
-    std::vector<edm::Handle<SKIROC2DigiCollection> >::iterator i;
-    for (i=ski.begin(); i!=ski.end(); i++) {
-      const SKIROC2DigiCollection& c=*(*i);
+	try {
+		e.getManyByType(ski);
+		std::vector<edm::Handle<SKIROC2DigiCollection> >::iterator i;
+		for (i = ski.begin(); i != ski.end(); i++) {
+			const SKIROC2DigiCollection& c = *(*i);
 
-      cout << "SKIROC2 Digis: " << i->provenance()->branchName() << endl;
-      
-      for (SKIROC2DigiCollection::const_iterator j=c.begin(); j!=c.end(); j++)
-        cout << "  " << *j << std::endl;
-    }
-  } catch (...) {
-    cout << "No SKIROC2 Digis." << endl;
-  }
+			cout << "SKIROC2 Digis: " << i->provenance()->branchName() << endl;
+
+			for (SKIROC2DigiCollection::const_iterator j = c.begin(); j != c.end(); j++)
+				cout << "  " << *j << std::endl;
+		}
+	} catch (...) {
+		cout << "No SKIROC2 Digis." << endl;
+	}
 }
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
