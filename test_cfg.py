455e52e88258369fb5fb39aad53e892d0d002cb8
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-04-03 18:32:13 +0200

    pedestal analysis in cfg

--- test_cfg.py
+++ test_cfg.py
@@ -3,44 +3,52 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("unpack")
 process.load('HGCal.RawToDigi.hgcaltbdigis_cfi')
 process.load('HGCal.RawToDigi.hgcaltbdigisplotter_cfi')
+process.load('HGCal.Calibration.pedestals_cfi')
 #process.load('HGCal.Reco.hgcaltbrechitproducer_cfi')
 #process.load('HGCal.Reco.hgcaltbrechitplotter_cfi')
 
 process.source = cms.Source("HGCalTBTextSource",
                             run=cms.untracked.int32(1), ### maybe this should be read from the file
-#                            fileNames=cms.untracked.vstring("file:Raw_data_New.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
-                            fileNames=cms.untracked.vstring("file:HGC_Output_51.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
+                            fileNames=cms.untracked.vstring("file:HGC_Output_51.txt") ### here a vector is provided
 )
 
-process.dumpRaw = cms.EDAnalyzer("DumpFEDRawDataProduct",
-                              dumpPayload=cms.untracked.bool(True))
 
-process.dumpDigi = cms.EDAnalyzer("HGCalDigiDump")
+process.TFileService = cms.Service("TFileService", fileName = cms.string("test_DigiAndRechitPlotter_TB_New.root") )
 
 process.output = cms.OutputModule("PoolOutputModule",
                                   compressionAlgorithm = cms.untracked.string('LZMA'),
                                   compressionLevel = cms.untracked.int32(4),
                                   dataset = cms.untracked.PSet(
-        dataTier = cms.untracked.string('USER'),
-        filterName = cms.untracked.string('')
-        ),
+                                      dataTier = cms.untracked.string('USER'),
+                                      filterName = cms.untracked.string('')
+                                  ),
                                   #dropMetaData = cms.untracked.string('ALL'),
                                   eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
-                                 fastCloning = cms.untracked.bool(False),
-                                 fileName = cms.untracked.string('test_output.root'), #options.output),
-#                                 outputCommands = process.MICROAODSIMEventContent.outputCommands,
-#                                 overrideInputFileSplitLevels = cms.untracked.bool(True),
-#                                 SelectEvents = SelectEventsPSet
-                                 )
+                                  fastCloning = cms.untracked.bool(False),
+                                  fileName = cms.untracked.string('test_output.root'), #options.output),
+                                  #                                 outputCommands = process.MICROAODSIMEventContent.outputCommands,
+                                  #                                 overrideInputFileSplitLevels = cms.untracked.bool(True),
+                                  #                                 SelectEvents = SelectEventsPSet
+                              )
+#============================================================ dumpers for debug
+process.dumpRaw = cms.EDAnalyzer("DumpFEDRawDataProduct",
+                                 dumpPayload=cms.untracked.bool(True)
+                             )
 
-process.TFileService = cms.Service("TFileService", fileName = cms.string("test_DigiAndRechitPlotter_TB_New.root") )
+process.dumpDigi = cms.EDAnalyzer("HGCalDigiDump")
 
+
+
+#============================================================ Sequences
+process.debugRawSeq = cms.Sequence(process.dumpRaw)
+process.RawToDigiSeq = cms.Sequence(process.hgcaltbdigis)
+process.DQMSeq = cms.Sequence(process.hgcaltbdigisplotter)
 
 #process.p =cms.Path(process.dumpRaw*process.hgcaltbdigis*process.dumpDigi*process.hgcaltbdigisplotter*process.hgcaltbrechits*process.hgcaltbrechitsplotter)
 
 #process.p =cms.Path(process.dumpRaw*process.hgcaltbdigis*process.dumpDigi*process.hgcaltbdigisplotter)
 
-process.p =cms.Path(process.dumpRaw*process.hgcaltbdigis*process.hgcaltbdigisplotter)
+process.p =cms.Path(process.debugRawSeq * process.RawToDigiSeq * process.DQMSeq * process.pedestals)
 
 process.end = cms.EndPath(process.output)
 
