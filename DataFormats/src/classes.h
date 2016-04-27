a397f652ff12eee54c2e68fee6ec88a549b02408
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-04-27 03:15:03 +0200

    FIX of the need to compile twice

--- DataFormats/src/classes.h
+++ DataFormats/src/classes.h
@@ -14,8 +14,8 @@ namespace DataFormats_HGCTB
 struct dictionary {
 	HGCalTBRecHit _aRecHit;
 	std::vector<HGCalTBRecHit> _HGCTBRHitVect;
-	edm::SortedCollection<HGCalTBRecHit> _theHGCTBRsc;
-	edm::Wrapper< HGCalTBRecHitCollection > _HGCTBeeRHitProd;
+//	edm::SortedCollection<HGCalTBRecHit> _theHGCTBRsc;
+//	edm::Wrapper< HGCalTBRecHitCollection > _HGCTBeeRHitProd;
 
 	SKIROC2DigiCollection _SR2DC;
 	edm::Wrapper<SKIROC2DigiCollection> _theSR2DC;
