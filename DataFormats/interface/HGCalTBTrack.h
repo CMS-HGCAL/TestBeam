a0237a31f58ef78be596474f88ff33207628bda7
Shervin <shervin@cern.ch>
2016-04-27 04:23:55 +0200

    Using 3D point and vector for internal representation and method to extrapolate to defined distance in z

--- DataFormats/interface/HGCalTBTrack.h
+++ DataFormats/interface/HGCalTBTrack.h
@@ -4,26 +4,53 @@
 #include <vector>
 #include <cstddef>
 
+#include "DataFormats/Math/interface/Error.h"
+#include "DataFormats/Math/interface/Vector3D.h"
+#include "DataFormats/Math/interface/Point3D.h"
+
+
 /** \class HGCal/DataFormats/interface/HGCalTBTrack.h HGCalTBTrack.h HGCalTBTrack
  * \brief Track from FNAL TB Telescope
  *
  */
 
-class HGCalTBTrack 
+class HGCalTBTrack
 {
-public: 
+public:
 	HGCalTBTrack(void); ///< default constructor
 	HGCalTBTrack(const float *raw); ///< constructor from FEDRawData: size is the number of floats in the array
 	std::vector<float> getRaw(void); ///< returns a vector of floats to be saved into FEDRawData
 
-	static inline unsigned int getSize(void) { return 7;};
-	static inline size_t getSizeof(void) { return sizeof(float) * getSize() ;};
+	static inline unsigned int getSize(void)
+	{
+		return 7;
+	};
+	static inline size_t getSizeof(void)
+	{
+		return sizeof(float) * getSize() ;
+	};
+
+	/// spatial vector
+	typedef math::XYZVector Vector;
+
+	/// point in the space
+	typedef math::XYZPoint Point;
+
+	inline const Point& vertex() const
+	{
+		return _vertex;
+	};
+	inline const Vector& momentum() const
+	{
+		return _momentum;
+	};
+
+	Point pointAt(double z) const;
 private:
+	Point _vertex; ///< intercept on the telescope plane (z = 0)
+	Vector _momentum; ///< m_x(z), m_y(z), 1
+
 	float chi2; ///< chi2 of the track
-	float x0;   ///< intercept: x on the telescope plane
-	float y0;   ///< intercept: y on the telescope plane
-	float m_x;  ///< x direction from telescope hits
-	float m_y;  ///< y direction from telescope hits
 	float m_x_err; ///< uncertainty on x-direction
 	float m_y_err; ///< uncertainty on y-direction
 
