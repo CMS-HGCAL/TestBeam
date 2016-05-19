f020856501179aca6907be9beaa254b0516ae51c
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-05-19 10:10:43 +0200

    Bug fixes

--- Geometry/interface/HGCalTBTopology.h
+++ Geometry/interface/HGCalTBTopology.h
@@ -1,6 +1,7 @@
 #ifndef HGCAL_GEOMETRY_HGCALTBTOPOLOGY_H
 #define HGCAL_GEOMETRY_HGCALTBTOPOLOGY_H 1
-
+#include <vector>
+#include "HGCal/DataFormats/interface/HGCalTBDetId.h"
 /** \class HGCalTBTopology
   *
   * Reference: https://indico.cern.ch/event/456955/
@@ -14,9 +15,9 @@ class HGCalTBTopology
 public:
 	// valid sensorSizes are 128 and 256
 	bool iu_iv_valid(int layer, int sensor_iu, int sensor_iv, int iu, int iv, int sensorSize) const;
-	inline bool isValidDetId(HGCalTBDetId detId) const
+	inline bool isValidDetId(HGCalTBDetId detId, int sensorSize) const
 	{
-		return iu_iv_valid(detId.layer(), detId.sensorIU(), detId.sensorIV(), detId.iu(), detId, iv());
+		return iu_iv_valid(detId.layer(), detId.sensorIU(), detId.sensorIV(), detId.iu(), detId.iv(), sensorSize);
 	};
 	double Cell_Area(int cell_type) const;//returns area in cm*cm
 };
