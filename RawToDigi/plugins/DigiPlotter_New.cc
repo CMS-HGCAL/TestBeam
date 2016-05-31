f56b57ec4fd8edd2931bb51fbcfaa051e78b2278
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-05-31 15:40:03 +0200

    changed digi plotter
    Fixed compilation errors and few cleanings

--- RawToDigi/plugins/DigiPlotter_New.cc
+++ RawToDigi/plugins/DigiPlotter_New.cc
@@ -35,18 +35,22 @@
 #include "HGCal/DataFormats/interface/HGCalTBRecHit.h"
 #include "HGCal/Geometry/interface/HGCalTBCellVertices.h"
 #include "HGCal/Geometry/interface/HGCalTBTopology.h"
+#include "HGCal/Geometry/interface/HGCalTBGeometryParameters.h"
 #include "CommonTools/UtilAlgos/interface/TFileService.h"
 #include "HGCal/DataFormats/interface/HGCalTBDataFrameContainers.h"
 #include "HGCal/CondObjects/interface/HGCalElectronicsMap.h"
 #include "HGCal/DataFormats/interface/HGCalTBElectronicsId.h"
+#include "HGCal/DataFormats/interface/SKIROCParameters.h"
+#include "HGCal/Geometry/interface/HGCalTBGeometryParameters.h"
 
 #define MAXVERTICES 6
-#define MAXLAYERS 4
+
 class Pedestal
 {
-	std::vector<std::pair<double, unsigned int> > _pedestals;
 public:
-	Pedestal(){};
+	std::vector<std::pair<double, unsigned int> > _pedestals;
+	Pedestal() {};
+
 	void Sum(HGCalTBDetId detId, double pedestal)
 	{
 		_pedestals[detId.cellType()].first += pedestal;
@@ -88,7 +92,7 @@ private:
 	void InitTH2Poly(TH2Poly& poly);
 
 	// ----------member data ---------------------------
-	Pedestal _pedestals[2]; // one set of pedestals per skiroc
+	Pedestal _pedestals[MAXLAYERS][2]; // one set of pedestals per skiroc
 	HGCalTBTopology IsCellValid;
 	HGCalTBCellVertices TheCell;
 	int sensorsize = 128;// The geometry for a 256 cell sensor hasnt been implemted yet. Need a picture to do this.
@@ -96,9 +100,13 @@ private:
 	HGCalTB::Point2D CellCentreXY;
 	std::vector<HGCalTB::Point2D>::const_iterator it;
 	const static int NSAMPLES = 2;
-	const static int NLAYERS  = 1;
 	edm::Service<TFileService> fs;
 
+	const static int NLAYERS  = 1;
+	int Sensor_Iu = 0;
+	int Sensor_Iv = 0;
+
+	unsigned int _DQMprescale;
 };
 
 //
@@ -117,6 +125,8 @@ DigiPlotter_New::DigiPlotter_New(const edm::ParameterSet& iConfig)
 	//now do what ever initialization is needed
 	usesResource("TFileService");
 	consumesMany<SKIROC2DigiCollection>();
+
+	_DQMprescale = iConfig.getParameter<unsigned int>("DQMprescale");
 }//contructor ends here
 
 
@@ -134,8 +144,9 @@ void DigiPlotter_New::InitTH2Poly(TH2Poly& poly)
 	double HexX[MAXVERTICES] = {0.};
 	double HexY[MAXVERTICES] = {0.};
 
-	std::vector<HGCalTBDetId> detIds = IsCellValid.validDetIds(0, 128); // hard coding that should be fixed	
-	for( auto detId : detIds){
+///\todo Rajdeep still using loop over iv and iu
+	std::vector<HGCalTBDetId> detIds = IsCellValid.validDetIds(0, 128); // hard coding that should be fixed
+	for( auto detId : detIds) {
 		CellXY = TheCell.GetCellCoordinatesForPlots(detId, sensorsize);
 		assert(CellXY.size() == 4 || CellXY.size() == 6);
 		unsigned int iVertex = 0;
@@ -158,132 +169,99 @@ void DigiPlotter_New::InitTH2Poly(TH2Poly& poly)
 void
 DigiPlotter_New::analyze(const edm::Event& event, const edm::EventSetup& setup)
 {
-	int evId = event.id().event() - 1;
-
 	using namespace edm;
-	using namespace std;
+
+	int evId = event.id().event() - 1; // starts from 0 because is used as index in DQM plots....
+	if(evId % _DQMprescale != 0) return;
+
 	std::vector<edm::Handle<SKIROC2DigiCollection> > ski;
 	event.getManyByType(ski);
-//        int Event = event.id().event();
-	if(!ski.empty()) {
 
-		std::vector<edm::Handle<SKIROC2DigiCollection> >::iterator i;
-//                int counter1=0, counter2=0;
-		for(i = ski.begin(); i != ski.end(); i++) {
-			const SKIROC2DigiCollection& Coll = *(*i);
+	if(ski.empty()) return;
+	assert(ski.size() == 1); // the histograms have the same name for every digi collection... it should be fixed
+
+	std::vector<edm::Handle<SKIROC2DigiCollection> >::iterator i;
+	for(i = ski.begin(); i != ski.end(); i++) {
+		const SKIROC2DigiCollection& Coll = *(*i);
 #ifdef DEBUG
-			if(DEBUG) cout << "SKIROC2 Digis: " << i->provenance()->branchName() << endl;
+		if(DEBUG) std::cout << "SKIROC2 Digis: " << i->provenance()->branchName() << std::endl;
 #endif
 
+		char name[300], title[300];
+		TH2Poly *lowGain_hpoly[MAXLAYERS], *highGain_hpoly[MAXLAYERS];
 
-			char name[300], title[300];
-			TH2Poly *lowGain_hpoly[MAXLAYERS], *highGain_hpoly[MAXLAYERS];
+		for(unsigned int iLayer = 0; iLayer < MAXLAYERS; ++iLayer) {
 
-			for(unsigned int iLayer = 0; iLayer < MAXLAYERS; ++iLayer) {
+			lowGain_hpoly[iLayer] = fs->make<TH2Poly>();
+			sprintf(name, "FullLayer_ADC%i_Layer%i_Event%i", 0, iLayer + 1, evId);
+			sprintf(title, "Sum of adc counts per cell for ADC%i Layer%i Event%i", 0, iLayer + 1, evId);
+			lowGain_hpoly[iLayer]->SetName(name);
+			lowGain_hpoly[iLayer]->SetTitle(title);
+			InitTH2Poly(*lowGain_hpoly[iLayer]);
 
-				lowGain_hpoly[iLayer] = fs->make<TH2Poly>();
-				sprintf(name, "FullLayer_ADC%i_Layer%i_Event%i", 0, iLayer + 1, evId);
-				sprintf(title, "Sum of adc counts per cell for ADC%i Layer%i Event%i", 0, iLayer + 1, evId);
-				lowGain_hpoly[iLayer]->SetName(name);
-				lowGain_hpoly[iLayer]->SetTitle(title);
-				InitTH2Poly(*lowGain_hpoly[iLayer]);
+			highGain_hpoly[iLayer] = fs->make<TH2Poly>();
+			sprintf(name, "FullLayer_ADC%i_Layer%i_Event%i", 1, iLayer + 1, evId);
+			sprintf(title, "Sum of adc counts per cell for ADC%i Layer%i Event%i", 1, iLayer + 1, evId);
+			highGain_hpoly[iLayer]->SetName(name);
+			highGain_hpoly[iLayer]->SetTitle(title);
+			InitTH2Poly(*highGain_hpoly[iLayer]);
+		}
 
-				highGain_hpoly[iLayer] = fs->make<TH2Poly>();
-				sprintf(name, "FullLayer_ADC%i_Layer%i_Event%i", 1, iLayer + 1, evId);
-				sprintf(title, "Sum of adc counts per cell for ADC%i Layer%i Event%i", 1, iLayer + 1, evId);
-				highGain_hpoly[iLayer]->SetName(name);
-				highGain_hpoly[iLayer]->SetTitle(title);
-				InitTH2Poly(*highGain_hpoly[iLayer]);
-			}
-
-			sprintf(name, "SumCellHist_SKI1_Event%i", evId);
-			sprintf(title, "Sum of adc counts of cells  Event%i", evId);
-			TH1F* Sum_Hist_cells_SKI1 = fs->make<TH1F>(name, title, 4096, 0 , 4095);
+		sprintf(name, "SumCellHist_SKI1_Event%i", evId);
+		sprintf(title, "Sum of adc counts of cells  Event%i", evId);
+		TH1F* Sum_Hist_cells_SKI1 = fs->make<TH1F>(name, title, 4096, 0 , 4095);
 
 //////////////////////////////////Evaluate average pedestal per event to subtract out//////////////////////////////////
-			for(SKIROC2DigiCollection::const_iterator k = Coll.begin(); k != Coll.end(); k++) {
-				const SKIROC2DataFrame& skiFrame = *k ;
-				HGCalTBDetId detId = skiFrame.detid();
-				unsigned int iSample=0; /// \todo now only using one sample, make it more general
-#ifdef DEBUG
-				if(DEBUG) cout << endl << " Layer = " << n_layer << " Sensor IU = " << n_sensor_IU << " Sensor IV = " << n_sensor_IV << " Cell iu = " << n_cell_iu << " Cell iu = " << n_cell_iv << endl;
-#endif
-				if(!IsCellValid.isValidDetId(detId, sensorsize))  continue;
+		for(SKIROC2DigiCollection::const_iterator skiFrame_itr = Coll.begin(); skiFrame_itr != Coll.end(); skiFrame_itr++) {
+			const SKIROC2DataFrame& skiFrame = *skiFrame_itr ;
+			HGCalTBDetId detId = skiFrame.detid();
+			unsigned int iSample = 0; /// \todo now only using one sample, make it more general
+			if(!IsCellValid.isValidDetId(detId, sensorsize))  continue;
 
-				CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots(detId, sensorsize);
-				double iux = (CellCentreXY.first < 0 ) ? (CellCentreXY.first + 0.0001) : (CellCentreXY.first - 0.0001) ;
-				double iyy = (CellCentreXY.second < 0 ) ? (CellCentreXY.second + 0.0001) : (CellCentreXY.second - 0.0001);
+			CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots(detId, sensorsize);
+			double iux = (CellCentreXY.first < 0 ) ? (CellCentreXY.first + 0.0001) : (CellCentreXY.first - 0.0001) ;
+			double iyy = (CellCentreXY.second < 0 ) ? (CellCentreXY.second + 0.0001) : (CellCentreXY.second - 0.0001);
 
-				_pedestals[detId.skiIndexInSensor()].Sum(detId, skiFrame[iSample].adcLow());
+			_pedestals[detId.layer()][detId.skiIndexInSensor()].Sum(detId, skiFrame[iSample].adcLow());
 
-
-				// if(detId().cellType() == kCellTypeCalibInner || detId.cellType() == kCellTypeCalibOuter) {
-				// 	Average_Pedestal_Calib_Cell_Event1 += skiFrame[iSample].adcLow();
-				// 	Cell_counter1_Calib_Cell1++;
-				// };
-
-
-				// if(((iux <= 0.25 && iyy >= -0.25 ) || (iux < -0.5)) && ((detId.cellType() == kCellHalf) || detId.cellType() == kCellMouseBite) ) {
-				// 	Average_Pedestal_Half_Cell_Event1 += skiFrame[iSample].adcLow();
-				// 	Cell_counter1_Half_Cell1++;
-				// };
-
-				// if(((iux > -0.25 && iyy < -0.50 ) || (iux > 0.50)) && ((detId.cellType() == kCellHalf) || detId.cellType() == kCellMouseBite )  ) {
-				// 	Average_Pedestal_Half_Cell_Event2 += skiFrame[iSample].adcLow();
-				// 	Cell_counter1_Half_Cell2++;
-				// };
-				if(((iux <= 0.25 && iyy >= -0.25 ) || (iux < -0.5 && iyy < 0)) && detId.cellType() == HGCalTBDetId::kCellTypeStandard) {
-					// 	Cell_counter1++;
-					// 	Average_Pedestal_Per_Event1 += skiFrame[iSample].adcLow();
-					Sum_Hist_cells_SKI1->Fill(skiFrame[iSample].adcLow());
-				}
-
-				// if(((iux > -0.25 && iyy < -0.50 ) || (iux > 0.50 && iyy < 0) ) && detId.cellType() == kCellTypeStandard) {
-				// 	Cell_counter2++;
-				// 	Average_Pedestal_Per_Event2 += skiFrame[iSample].adcLow();
-				// }
-
-
+			if(((iux <= 0.25 && iyy >= -0.25 ) || (iux < -0.5 && iyy < 0)) && detId.cellType() == HGCalTBDetId::kCellTypeStandard) {
+//						std::cout << std::endl << iux*iyy << std::endl;
+				Sum_Hist_cells_SKI1->Fill(skiFrame[iSample].adcLow());// the histogram is defined only according to the DQM prescale
 			}
+		}
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 
-			for(SKIROC2DigiCollection::const_iterator j = Coll.begin(); j != Coll.end(); j++) {
-				const SKIROC2DataFrame& SKI = *j ;
+		for(SKIROC2DigiCollection::const_iterator skiFrame_itr = Coll.begin(); skiFrame_itr != Coll.end(); skiFrame_itr++) {
+			const SKIROC2DataFrame& SKI = *skiFrame_itr ;
 
-				int iSample = 0; // using only 1 sample ///\todo to be generalized
+			int iSample = 0; // using only 1 sample ///\todo to be generalized
 
-				HGCalTBDetId detId = SKI.detid();
-				if(!IsCellValid.isValidDetId(detId, sensorsize)) continue;
+			HGCalTBDetId detId = SKI.detid();
+			if(!IsCellValid.isValidDetId(detId, sensorsize)) continue;
 
+			CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots(detId, sensorsize);
 
-#ifdef DEBUG
-				if(DEBUG) cout << endl << " Layer = " << n_layer << " Sensor IU = " << n_sensor_IU << " Sensor IV = " << n_sensor_IV << " Cell iu = " << n_cell_iu << " Cell iu = " << n_cell_iv << endl;
-#endif
+			double iux = (CellCentreXY.first < 0 ) ? (CellCentreXY.first + 0.0001) : (CellCentreXY.first - 0.0001) ;
+			double iyy = (CellCentreXY.second < 0 ) ? (CellCentreXY.second + 0.0001) : (CellCentreXY.second - 0.0001);
 
-
-				CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots(detId, sensorsize);
-
-				double iux = (CellCentreXY.first < 0 ) ? (CellCentreXY.first + 0.0001) : (CellCentreXY.first - 0.0001) ;
-				double iyy = (CellCentreXY.second < 0 ) ? (CellCentreXY.second + 0.0001) : (CellCentreXY.second - 0.0001);
-
-				if(detId.cellType() == HGCalTBDetId::kCellTypeCalibOuter || detId.cellType() == HGCalTBDetId::kCellTypeCalibInner) {
-					lowGain_hpoly[detId.layer()]->Fill(iux, iyy, 0.5 * (SKI[iSample].adcLow() - _pedestals[detId.skiIndexInSensor()].GetPedestal(detId)));
-				} else {
-					lowGain_hpoly[detId.layer()]->Fill(iux, iyy, (SKI[iSample].adcLow() - _pedestals[detId.skiIndexInSensor()].GetPedestal(detId)));
-				}
-#ifdef DEBUG
-				if(SKI.detid().cellType() == HGCalDetId::kCellMouseBite && event.id().event() == 34) cout << endl << " iux= " << iux << " iyy= " << iyy << " ADC = " << SKI[iSample].adcLow() << " Ped= " << Average_Pedestal_Half_Cell_Event1 / (Cell_counter1_Half_Cell1) << endl;
-#endif
-
-				highGain_hpoly[detId.layer()]->Fill(iux , iyy, (SKI[iSample].adcHigh() - Sum_Hist_cells_SKI1->GetMean()));
+			if(detId.cellType() == HGCalTBDetId::kCellTypeCalibOuter || detId.cellType() == HGCalTBDetId::kCellTypeCalibInner) { //1 and 4
+				lowGain_hpoly[detId.layer()]->Fill(iux, iyy, 0.5 * (SKI[iSample].adcLow()));
+//lowGain_hpoly[detId.layer()]->Fill(iux, iyy, 0.5 * (SKI[iSample].adcLow() - _pedestals[detId.skiIndexInSensor()].GetPedestal(detId)));
+			} else {
+				lowGain_hpoly[detId.layer()]->Fill(iux, iyy, (SKI[iSample].adcLow() ));
+//					lowGain_hpoly[detId.layer()]->Fill(iux, iyy, (SKI[iSample].adcLow() - _pedestals[detId.skiIndexInSensor()].GetPedestal(detId)));
 			}
-		}
-	} else {
-		edm::LogWarning("DQM") << "No SKIROC2 Digis";
-	}
+#ifdef DEBUG
+			if(SKI.detid().cellType() == HGCalDetId::kCellMouseBite && event.id().event() == 34) std::cout << std::endl << " iux= " << iux << " iyy= " << iyy << " ADC = " << SKI[iSample].adcLow() << " Ped= " << Average_Pedestal_Half_Cell_Event1 / (Cell_counter1_Half_Cell1) << std::endl;
+#endif
 
-}//analyze method ends here
+			highGain_hpoly[detId.layer()]->Fill(iux , iyy, (SKI[iSample].adcHigh() - _pedestals[detId.layer()][detId.skiIndexInSensor()].GetPedestal(detId)));
+		}
+	}
+}	//analyze method ends here
 
 
 // ------------ method called once each job just before starting event loop  ------------
@@ -301,7 +279,7 @@ DigiPlotter_New::endJob()
 
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
-DigiPlotter_New::fillDescriptions(edm::ConfigurationDescriptions& descriptions)
+DigiPlotter_New::fillDescriptions(edm::ConfigurationDescriptions & descriptions)
 {
 	//The following says we do not know what parameters are allowed so do no validation
 	// Please change this to state exactly what you do use, even if it is no parameters
