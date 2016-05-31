ff40facc2f0310f9b74d36c448dbafad2305031b
Shervin Nourbakhsh <shervin.nourbakhsh@cern.ch>
2016-05-31 15:38:43 +0200

    Completely different RawToDigi

--- RawToDigi/plugins/HGCalTBRawToDigi.cc
+++ RawToDigi/plugins/HGCalTBRawToDigi.cc
@@ -1,12 +1,12 @@
-//#include <iostream>
+#include <iostream>
 #include "HGCal/RawToDigi/plugins/HGCalTBRawToDigi.h"
-
+using namespace std;
 
 unsigned int gray_to_binary (unsigned int gray);
-
+int counter = 0;
 HGCalTBRawToDigi::HGCalTBRawToDigi(edm::ParameterSet const& conf):
 	dataTag_(conf.getParameter<edm::InputTag>("InputLabel")),
-	fedIds_(conf.getUntrackedParameter<std::vector<int> >("fedIds")),
+	fedId_(conf.getUntrackedParameter<int>("fedId")),
 	mapfile_(conf.getUntrackedParameter<std::string>("electronicsMap"))
 {
 	produces<SKIROC2DigiCollection>();
@@ -30,39 +30,55 @@ void HGCalTBRawToDigi::produce(edm::Event& e, const edm::EventSetup& c)
 	edm::Handle<FEDRawDataCollection> rawraw;
 	e.getByLabel(dataTag_, rawraw);
 
-	std::auto_ptr<SKIROC2DigiCollection> digis = std::auto_ptr<SKIROC2DigiCollection>(new SKIROC2DigiCollection(SKIROC::MAXSAMPLES)); // 2 because TDC and ADC
+	std::auto_ptr<SKIROC2DigiCollection> digis(0);
 	//
-
-	for(auto fedId_ : fedIds_) {
-
-		const FEDRawData& fed = rawraw->FEDData(fedId_);
-		if(fed.size() == 0) continue; // empty FEDs are allowed: for example when one FED is not in the readout for a particular run
-
+	const FEDRawData& fed = rawraw->FEDData(fedId_);
+	if (fed.size() != 0) { /// \todo Exception if 0????
+		int ski_up = 2;
+		int ski_down = 1;
 		// we can figure out the number of samples from the size of the raw data
-		//int nsamples = fed.size() / (sizeof(uint16_t) * SKIROC::NCHANNELS * 2); // 2 is for ADC and TDC
-		//assert(nsamples>0);
-
+		int nsamples = fed.size() / (sizeof(uint16_t) * SKIROC::NCHANNELS * 2); // 2 is for ADC and TDC
+		digis = std::auto_ptr<SKIROC2DigiCollection>(new SKIROC2DigiCollection(nsamples));
 		const uint16_t* pdata = (const uint16_t*)(fed.data());
 
 		// we start from the back...
 		int ptr = fed.size() / sizeof(uint16_t) - 1;
-		for (int ski = 2; ski >= 1; ski--) { // starting from 2 because then we are going back reading two samples
-			for (int ichan = 0; ichan < SKIROC::NCHANNELS; ichan++) {
+		/*
+				printf("Starting on SKIROC %x\n", pdata[ptr]);
+				ptr--; // now we are pointing at a relatively-useless header word
+				ptr--; // now we are pointing at the first TDC word
+		*/
+		counter++;
+		if(counter > 502) {
+			ski_up = 4;
+			ski_down = 3;
+		}
+//                for (int ski = 2; ski >= 1; ski--) {
+		for (int ichan = 0; ichan < SKIROC::NCHANNELS; ichan++) {
+			for (int ski = ski_up; ski >= ski_down; ski--) {
 				HGCalTBElectronicsId eid(ski, ichan);
 				if (!essource_.emap_.existsEId(eid.rawId())) {
-					std::cout << "We do not have a mapping for " << eid << std::endl;
+					std::cout << "We do not have a mapping for " << eid;
 				} else {
 					HGCalTBDetId did = essource_.emap_.eid2detId(eid);
 					digis->addDataFrame(did);
-					int ptradc1 = ptr - ichan - 128 * (2 - ski);
-					int ptradc2 = ptr - ichan - 64 - 128 * (2 - ski);
-					digis->backDataFrame().setSample(0, gray_to_binary(pdata[ptradc1] & 0xFFF), gray_to_binary( pdata[ptradc2] & 0xFFF), 0); ///\todo TDC value hardcoded to 0 because not in the readout
+					if(ski <= 2) {
+						int ptradc1 = ptr - ichan * 2 - (ski_up - ski);
+						int ptradc2 = ptr - ichan * 2 - (ski_up - ski) -  128;
+						digis->backDataFrame().setSample(0, gray_to_binary(pdata[ptradc1] & 0xFFF), gray_to_binary( pdata[ptradc2] & 0xFFF), 0);
+						cout << endl << dec << "SKI= " << ski << " chan= " << ichan << " " << ptradc1 << " " << ptradc2 << " " << " High= " << hex << (pdata[ptradc1]) << dec << "  " << gray_to_binary(pdata[ptradc1] & 0xFFF) << " Low= " << hex << ( pdata[ptradc2] & 0xFFF) << "  " << gray_to_binary( pdata[ptradc2] & 0xFFF) << endl;
+					} else {
+						int ptradc1 = ptr - ichan * 2 + (ski_up - ski) ;
+						int ptradc2 = ptr - ichan * 2 + (ski_up - ski) -  128;
+						digis->backDataFrame().setSample(0, gray_to_binary(pdata[ptradc1] & 0xFFF), gray_to_binary( pdata[ptradc2] & 0xFFF), 0);
+
+					}
 				}
-			}
+			}//loop over skirocs
 		}
+//                 }
 
-	}
-
+	}// fed size > 0
 
 	// put it into the event
 	e.put(digis);
