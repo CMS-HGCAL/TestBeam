d55e1d0c198ea95968e216ea9f36469e452358b9
Rajdeep <r.m.chatterjeejr@gmail.com>
2016-07-29 17:04:25 +0200

    Rec Hits

--- test_cfg.py
+++ test_cfg.py
@@ -9,7 +9,7 @@ process.load('HGCal.Reco.hgcaltbrechitplotter_cfi')
 process.source = cms.Source("HGCalTBTextSource",
                             run=cms.untracked.int32(1), ### maybe this should be read from the file
 #                            fileNames=cms.untracked.vstring("file:Raw_data_New.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
-                            fileNames=cms.untracked.vstring("file:/afs/cern.ch/user/r/rchatter/public/data/HGC_Output_537.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
+                            fileNames=cms.untracked.vstring("file:/afs/cern.ch/user/r/rchatter/public/data/HGC_Output_563.txt") ### here a vector is provided, but in the .cc only the first one is used TO BE FIXED
 )
 
 process.dumpRaw = cms.EDAnalyzer("DumpFEDRawDataProduct",
@@ -33,15 +33,16 @@ process.output = cms.OutputModule("PoolOutputModule",
 #                                 SelectEvents = SelectEventsPSet
                                  )
 
-#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_537_Digi.root") )
-#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_537_Reco.root") )
-process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_537_Reco_Layer.root") )
-#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_537_Reco_Display.root") )
+#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_563_Reco_Display.root") )
+#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_563_Digi.root") )
+process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_563_Reco.root") )
+#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_563_Reco_Layer.root") )
+#process.TFileService = cms.Service("TFileService", fileName = cms.string("HGC_Output_563_Reco_Cluster.root") )
 
 
 #process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_new)
 #process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbdigisplotter)
-#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_correlation_cm)
-process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.FourLayerRecHitPlotterMax)
-
+process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.hgcaltbrechitsplotter_highgain_correlation_cm)
+#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.FourLayerRecHitPlotterMax)
+#process.p =cms.Path(process.hgcaltbdigis*process.hgcaltbrechits*process.LayerSumAnalyzer)
 process.end = cms.EndPath(process.output)
