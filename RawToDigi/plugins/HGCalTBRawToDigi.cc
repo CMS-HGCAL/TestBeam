bdab5be9042c10df5b3d6892467a7cf8b5de7935
Rajdeep <r.m.chatterjeejr@gmail.com>
2016-09-01 17:43:25 +0200

    Cleaning up Event display and RawToDigi

--- RawToDigi/plugins/HGCalTBRawToDigi.cc
+++ RawToDigi/plugins/HGCalTBRawToDigi.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "HGCal/RawToDigi/plugins/HGCalTBRawToDigi.h"
 #include "HGCal/Geometry/interface/HGCalTBGeometryParameters.h"
+#include "HGCal/Geometry/interface/HGCalTBSpillParameters.h"
 using namespace std;
 
 unsigned int gray_to_binary (unsigned int gray);
@@ -50,69 +51,69 @@ void HGCalTBRawToDigi::produce(edm::Event& e, const edm::EventSetup& c)
 		ptr--; // now we are pointing at the first TDC word
 */
          counter++;
-         if(counter > 150){
+         if(counter > EVENTSPERSPILL){
             ski_up = 4;
             ski_down = 3;
            }  
  
-         if(counter > 150*2 ){
+         if(counter > EVENTSPERSPILL*2 ){
             ski_up = 6;
             ski_down = 5;  
            }
-         if(counter > 150*3){
+         if(counter > EVENTSPERSPILL*3){
             ski_up = 8;
             ski_down = 7;
            }
-         if(counter > 150*4){
+         if(counter > EVENTSPERSPILL*4){
             ski_up = 10;
             ski_down = 9;
            }
-         if(counter > 150*5){
+         if(counter > EVENTSPERSPILL*5){
             ski_up = 12;
             ski_down = 11;
            }
-         if(counter > 150*6){
+         if(counter > EVENTSPERSPILL*6){
             ski_up = 14;
             ski_down = 13;
            }
-         if(counter > 150*7){
+         if(counter > EVENTSPERSPILL*7){
             ski_up = 16;
             ski_down = 15;
            }
-         if(counter > 150*8 ){
+         if(counter > EVENTSPERSPILL*8 ){
             ski_up = 18;
             ski_down = 17;
            }
-         if(counter > 150*9){
+         if(counter > EVENTSPERSPILL*9){
             ski_up = 20;
             ski_down = 19;
            }
-         if(counter > 150*10){
+         if(counter > EVENTSPERSPILL*10){
             ski_up = 22;
             ski_down = 21;
            }
-         if(counter > 150*11 ){
+         if(counter > EVENTSPERSPILL*11 ){
             ski_up = 24;
             ski_down = 23;
            }
-         if(counter > 150*12){
+         if(counter > EVENTSPERSPILL*12){
             ski_up = 26;
             ski_down = 25;
            }
-	 if(counter > 150*13){
+	 if(counter > EVENTSPERSPILL*13){
             ski_up = 28;
 	    ski_down = 27;		
 	   }
-         if(counter > 150*14){
+         if(counter > EVENTSPERSPILL*14){
             ski_up = 30;
             ski_down = 29;
            }
-         if(counter > 150*15){
+         if(counter > EVENTSPERSPILL*15){
             ski_up = 32;
             ski_down = 31;
            }
 
-         if(counter == 150*MAXLAYERS) counter = 0;   
+         if(counter == EVENTSPERSPILL*MAXLAYERS) counter = 0;   
 //                for (int ski = 2; ski >= 1; ski--) {
 			for (int ichan = 0; ichan < SKIROC::NCHANNELS; ichan++) {
                             for (int ski = ski_down; ski <= ski_up; ski++) {
