13c41eced48bbf46cbf801bc2fe68e41ef26b403
Shervin <shervin@cern.ch>
2016-09-27 15:29:51 +0200

    New text source

--- RawToDigi/plugins/HGCalTBTextSource.h
+++ RawToDigi/plugins/HGCalTBTextSource.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <fstream>
 
+#define MAX_LAYERS 16
 /**
  * \class HGCalTBTextSource HGCal/RawToDigi/plugins/HGCalTBTextSource.h
  *
@@ -22,9 +23,7 @@ class HGCalTBTextSource : public edm::ProducerSourceFromFiles
 
 public:
 	explicit HGCalTBTextSource(const edm::ParameterSet & pset, edm::InputSourceDescription const& desc) :  edm::ProducerSourceFromFiles(pset, desc, true),
-		m_file(0),
-		m_run(pset.getUntrackedParameter<int>("run", 101)), /// \todo check and read from file?
-		m_nSpills(pset.getUntrackedParameter<unsigned int>("nSpills", 15))
+		m_file(0)
 	{
 
 		m_sourceId = pset.getUntrackedParameter<int>("fed", 1000); /// \todo check and read from file?
@@ -42,12 +41,12 @@ public:
 private:
 	bool setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& time, edm::EventAuxiliary::ExperimentType&);
 	virtual void produce(edm::Event & e);
+	bool readHeader(void);
 	bool readLines(void);
 
-	std::vector<std::string> m_lines;
+	std::array< std::vector < unsigned int> , MAX_LAYERS> m_lines;
 	FILE* m_file;
-	unsigned int m_time, m_time_tmp, m_run_tmp;
-	int m_event, m_run;
-	unsigned int m_nSpills;
+	unsigned int m_time;
+	unsigned int m_event, m_run, m_spill, max_boards;
 	int m_sourceId;
 };
