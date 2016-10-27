6acc217badf801b3c6e0e3dd56820914103131c7
arabella <arabella.martelli@cern.ch>
2016-10-27 09:02:22 +0200

    conflict solved

--- test_cfg_newEB.py
+++ test_cfg_newEB.py
@@ -6,11 +6,7 @@ import os,sys
 options = VarParsing.VarParsing('standard') # avoid the options: maxEvents, files, secondaryFiles, output, secondaryOutput because they are already defined in 'standard'
 
 options.register('dataFolder',
-                 #'/afs/cern.ch/work/r/rchatter/Final_Event_Builder/CMSSW_8_0_1/src/HGCal/tmpOut/',
-                 #'/afs/cern.ch/user/a/amartell/public/HGCal/TB_data/'
-                 #'/tmp/amartell/rearrangedTxtFiles/',
-                 '/afs/cern.ch/user/a/amartell/eos/cms/store/group/upgrade/HGCAL/TestBeam/CERN/Sept2016/',
-                 #'~/eos/cms/store/group/upgrade/HGCAL/TestBeam/CERN/Sept2016/',
+                 '~/eos/cms/store/group/upgrade/HGCAL/TestBeam/CERN/Sept2016/',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  'folder containing raw text input')
