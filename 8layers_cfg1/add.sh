42f702c62539cbf433edfb5569d9d2dba38afedc
arabella <arabella.martelli@cern.ch>
2016-11-09 19:49:08 +0100

    new shapes

---
+++ 8layers_cfg1/add.sh
@@ -0,0 +1,6 @@
+add 20GeV.root      934.root
+add 32.root 1092.root 1095.root 1100.root 1101.root 1102.root
+add 70.root 870.root 874.root 879.root 884.root 887.root
+add 100.root 1052.root 1053.root 1057.root 1059.root 1060.root 1069.root 1070.root 1072.root 1073.root 
+add 200.root 1028.root 1030.root 1037.root 1039.root 1042.root 1047.root
+add 250.root 942.root 955.root 959.root
---
+++ 8layers_cfg2/add.sh
@@ -0,0 +1,7 @@
+add 20GeV.root 1255.root 1256.root 1259.root 1262.root 1265.root 1266.root
+add 32.root 1226.root 1229.root 1230.root 1231.root 1236.root 1237.root 1238.root 1239.root 1241.root 1245.root
+
+#add 70.root 870.root 874.root 879.root 884.root 887.root
+add 100.root 1205.root 1213.root 1218.root
+add 200.root 1182.root 1183.root 1184.root 1196.root 1198.root
+#add 250.root 942.root 955.root 959.root
