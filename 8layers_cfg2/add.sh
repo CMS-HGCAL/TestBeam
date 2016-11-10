223aa00b503d54c4b35ae6d2310b5de13db9e68f
arabella <arabella.martelli@cern.ch>
2016-11-10 10:42:36 +0100

    final scripts for analysis

---
+++ 8layers_cfg2/add.sh
@@ -0,0 +1,7 @@
+add 20GeV.root 1255.root 1256.root 1259.root 1262.root 1265.root 1266.root
+add 32.root 1226.root 1229.root 1230.root 1231.root 1236.root 1237.root 1238.root 1239.root 1241.root 1245.root
+add 70.root 1155.root 1158.root 1159.root 1160.root 1170.root 1171.root
+add 100.root 1205.root 1213.root 1218.root
+add 200.root 1182.root 1183.root 1184.root 1196.root 1198.root
+add 250.root 1293.root 1300.root 1304.root 1306.root 1310.root
+
---
+++ 8layers_cfg2/lanciaAll.sh
@@ -0,0 +1,2 @@
+source lancia100GeV.sh lancia200GeV.sh lancia20GeV.sh lancia250GeV.sh lancia32GeV.sh lancia70GeV.sh
+
