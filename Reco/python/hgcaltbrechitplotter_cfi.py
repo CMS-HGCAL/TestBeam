465097511f4d0db9e3ff51e17cb6c6e5d7987908
thorben quast <thorben.quast@cern.ch>
2017-03-08 09:45:05 +0100

    [recHitPlotter] add RUNDATA to the configuration

--- Reco/python/hgcaltbrechitplotter_cfi.py
+++ Reco/python/hgcaltbrechitplotter_cfi.py
@@ -39,6 +39,7 @@ FourLayerRecHitPlotterMax = cms.EDAnalyzer("FourLayerRecHitPlotterMax",
 
 LayerSumAnalyzer = cms.EDAnalyzer("Layer_Sum_Analyzer",
                                   HGCALTBRECHITS = cms.InputTag("hgcaltbrechits","","unpack" ),
+                                  RUNDATA = cms.InputTag("source","RunData","unpack" ),
                                   layers_config = cms.int32(-1),
                                   mapFile_CERN = cms.string('HGCal/CondObjects/data/map_CERN_8Layers_Sept2016.txt'),
                                   mapFile_FNAL = cms.string('')
