44bea199cc480049d62d23f45e5929ecf9cb4084
asteencern <arnaud.steen@cern.ch>
2017-05-30 12:58:02 +0200

    add PulseShapePlotter

--- run2017_cfg.py
+++ run2017_cfg.py
@@ -6,8 +6,6 @@ import os,sys
 options = VarParsing.VarParsing('standard') # avoid the options: maxEvents, files, secondaryFiles, output, secondaryOutput because they are already defined in 'standard'
 #Change the data folder appropriately to where you wish to access the files from:
 options.register('dataFolder',
-                 #'./',
-								 #'/afs/cern.ch/work/a/asteen/public/data/may2017/raw',
                  '/afs/cern.ch/work/b/barneyd/public/data_May_2017/disk2_2TB/eudaq_data',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
@@ -17,16 +15,16 @@ options.register('runNumber',
                  850,
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.int,
-                 'Input file to process')
+                 'Input run to process')
 
 options.register('outputFolder',
                  '/afs/cern.ch/work/a/asteen/public/data/may2017/',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
-                 'Result of processing')
+                 'Output folder where analysis output are stored')
 
 options.maxEvents = -1
-options.output = "toto.root"
+options.output = "cmsswEvents.root"
 
 options.parseArguments()
 print options
@@ -40,13 +38,10 @@ process.maxEvents = cms.untracked.PSet(
 )
 
 ####################################
-#process.load('HGCal.StandardSequences.RawToDigi_cff')
 
 process.source = cms.Source("HGCalTBRawDataSource",
-                            #FilePath=cms.untracked.string(options.dataFolder),
-                            #FileName=cms.untracked.string("file:HexaData_Run00%d.raw"%(options.runNumber)),
                             ElectronicMap=cms.untracked.string("HGCal/CondObjects/data/map_CERN_Hexaboard_OneLayers_May2017.txt"),
-                            fileNames=cms.untracked.vstring("file:%s/HexaData_Run%04d.raw"%(options.dataFolder,options.runNumber)),
+                            fileNames=cms.untracked.vstring("file:%s/HexaData_Run%04d.raw"%(options.dataFolder,options.runNumber)),#the current version can handle only one file
                             OutputCollectionName=cms.untracked.string("skiroc2cmsdata"),
                             NOrmBoards=cms.untracked.uint32(1),
                             NHexaBoards=cms.untracked.uint32(1),
@@ -55,7 +50,7 @@ process.source = cms.Source("HGCalTBRawDataSource",
                             NumberOf32BitsWordsPerReadOut=cms.untracked.uint32(30788)
 )
 
-process.TFileService = cms.Service("TFileService", fileName = cms.string("%s/toto_%d.root"%(options.outputFolder,options.runNumber)))
+process.TFileService = cms.Service("TFileService", fileName = cms.string("%s/HexaOutput_%d.root"%(options.outputFolder,options.runNumber)))
 
 process.output = cms.OutputModule("PoolOutputModule",
                                   fileName = cms.untracked.string(options.output)
@@ -67,7 +62,7 @@ pedestalLowGain="pedestalLG_125.txt"
 
 process.rawdataplotter = cms.EDAnalyzer("RawDataPlotter",
                                         SensorSize=cms.untracked.int32(128),
-                                        EventPlotter=cms.untracked.bool(True),
+                                        EventPlotter=cms.untracked.bool(False),
                                         InputCollection=cms.InputTag("source","skiroc2cmsdata"),
                                         )
 process.content = cms.EDAnalyzer("EventContentAnalyzer") #add process.content in cms.Path if you want to check which collections are in the event
@@ -82,18 +77,24 @@ process.rawhitplotter = cms.EDAnalyzer("RawHitPlotter",
                                        ElectronicMap=cms.untracked.string("HGCal/CondObjects/data/map_CERN_Hexaboard_OneLayers_May2017.txt"),
                                        SensorSize=cms.untracked.int32(128),
                                        EventPlotter=cms.untracked.bool(False),
-                                       SubtractPedestal=cms.untracked.bool(True),
-                                       SubtractCommonMode=cms.untracked.bool(True),
+                                       SubtractPedestal=cms.untracked.bool(False),
+                                       SubtractCommonMode=cms.untracked.bool(False),
                                        HighGainPedestalFileName=cms.string(pedestalHighGain),
                                        LowGainPedestalFileName=cms.string(pedestalLowGain)
                                        )
 
+process.pulseshapeplotter = cms.EDAnalyzer("PulseShapePlotter",
+                                           InputCollection=cms.InputTag("rawhitproducer","HGCALTBRAWHITS"),
+                                           ElectronicMap=cms.untracked.string("HGCal/CondObjects/data/map_CERN_Hexaboard_OneLayers_May2017.txt"),
+                                           HighGainPedestalFileName=cms.string(pedestalHighGain),
+                                           LowGainPedestalFileName=cms.string(pedestalLowGain)
+                                           )
 
 
 
 #process.p = cms.Path( process.rawdataplotter )
 process.p = cms.Path( process.rawhitproducer*process.rawhitplotter )
+#process.p = cms.Path( process.rawdataplotter*process.rawhitproducer*process.rawhitplotter*process.pulseshapeplotter )
 
 process.end = cms.EndPath(process.output)
 
-#help(process)
