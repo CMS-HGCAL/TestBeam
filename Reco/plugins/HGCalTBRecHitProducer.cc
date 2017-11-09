9655a43b07e12b65c3d5b5a039806e3cab97b662
asteencern <arnaud.steen@cern.ch>
2017-11-09 16:59:27 +0100

    adapt HGCalTBRecHitProducer with new calibration procedure; adapt RecHitPlotter

--- Reco/plugins/HGCalTBRecHitProducer.cc
+++ Reco/plugins/HGCalTBRecHitProducer.cc
@@ -10,18 +10,12 @@ const static int SENSORSIZE = 128;
 HGCalTBRecHitProducer::HGCalTBRecHitProducer(const edm::ParameterSet& cfg) : 
   m_outputCollectionName(cfg.getParameter<std::string>("OutputCollectionName")),
   m_electronicMap(cfg.getUntrackedParameter<std::string>("ElectronicsMap","HGCal/CondObjects/data/map_CERN_Hexaboard_28Layers_AllFlipped.txt")),
-  m_highGainADCSaturation(cfg.getUntrackedParameter<double>("HighGainADCSaturation",1800)),
-  m_lowGainADCSaturation(cfg.getUntrackedParameter<double>("LowGainADCSaturation",1800)),
+  m_detectorLayoutFile(cfg.getUntrackedParameter<std::string>("DetectorLayout","HGCal/CondObjects/data/layerGeom_oct2017_h2_17layers.txt")),
+  m_adcCalibrationsFile(cfg.getUntrackedParameter<std::string>("ADCCalibrations","HGCal/CondObjects/data/hgcal_calibration.txt")),
   m_timeSample3ADCCut(cfg.getUntrackedParameter<double>("TimeSample3ADCCut",15))
 {
   m_HGCalTBRawHitCollection = consumes<HGCalTBRawHitCollection>(cfg.getParameter<edm::InputTag>("InputCollection"));
-
   produces <HGCalTBRecHitCollection>(m_outputCollectionName);
-  std::vector<double> v0(1,10.);
-  m_LG2HG_value = cfg.getUntrackedParameter<std::vector<double> >("LG2HG",v0);
-  std::vector<double> v1(1,10.);
-  m_TOT2LG_value = cfg.getUntrackedParameter<std::vector<double> >("TOT2LG",v1);
-
   std::cout << cfg.dump() << std::endl;
 }
 
@@ -32,6 +26,18 @@ void HGCalTBRecHitProducer::beginJob()
   if (!io.load(fip.fullPath(), essource_.emap_)) {
     throw cms::Exception("Unable to load electronics map");
   };
+  fip=edm::FileInPath(m_detectorLayoutFile);
+  if (!io.load(fip.fullPath(), essource_.layout_)) {
+    throw cms::Exception("Unable to load detector layout file");
+  };
+  for( auto layer : essource_.layout_.layers() )
+    layer.print();
+  
+  fip=edm::FileInPath(m_adcCalibrationsFile);
+  if (!io.load(fip.fullPath(), essource_.adccalibmap_)) {
+    throw cms::Exception("Unable to load ADC conversions map");
+  };
+  //  std::cout << essource_.adccalibmap_ << std::endl;
 }
 
 void HGCalTBRecHitProducer::produce(edm::Event& event, const edm::EventSetup& iSetup)
@@ -47,29 +53,26 @@ void HGCalTBRecHitProducer::produce(edm::Event& event, const edm::EventSetup& iS
 
   std::vector<std::pair<double, double> > CellXY;
 
+  PulseFitter fitter(0,150);
+  PulseFitterResult fitresult;
   for( auto rawhit : *rawhits ){
     HGCalTBElectronicsId eid( essource_.emap_.detId2eid(rawhit.detid().rawId()) );
     if( !essource_.emap_.existsEId(eid) ) continue;
-    int iboard=rawhit.detid().layer()-1;
     int iski=eid.iskiroc();
-    //std::cout << iboard << " " << iski << std::endl;
 
     std::vector<double> sampleHG, sampleLG, sampleT;
 
-    float highGain(0), lowGain(0), totGain(0);
-    int hgStatus = -1;
-    int lgStatus = -1;
-    float timeHG = 0.;
-    float timeLG = 0.;
+    float highGain(0), lowGain(0), totGain(0), energy(0), time(-1);
     float subHG[NUMBER_OF_TIME_SAMPLES],subLG[NUMBER_OF_TIME_SAMPLES];
 
     totGain = rawhit.totSlow();
 
-    for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-      subHG[it]=0;
-      subLG[it]=0;
-    }
     switch ( rawhit.detid().cellType() ){
+    default :
+      for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+	subHG[it]=0;
+	subLG[it]=0;
+      }
     case 0 : 
       for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
 	subHG[it]=cmMap[iski].fullHG[it]; 
@@ -77,92 +80,82 @@ void HGCalTBRecHitProducer::produce(edm::Event& event, const edm::EventSetup& iS
       }
       break;
     case 2 : 
-      	for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-      	  subHG[it]=cmMap[iski].halfHG[it]; 
-      	  subLG[it]=cmMap[iski].halfLG[it]; 
-      	}
-      	break;
+      for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+	subHG[it]=cmMap[iski].halfHG[it]; 
+	subLG[it]=cmMap[iski].halfLG[it]; 
+      }
+      break;
     case 3 : 
       for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
 	subHG[it]=cmMap[iski].mouseBiteHG[it]; 
 	subLG[it]=cmMap[iski].mouseBiteLG[it]; 
       }
       break;
-      case 4 : for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+    case 4 : for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
 	subHG[it]=cmMap[iski].outerHG[it]; 
 	subLG[it]=cmMap[iski].outerLG[it]; 
       }
-       	break;
+      break;
+    case 5 : for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+	subHG[it]=cmMap[iski].mergedHG[it]; 
+	subLG[it]=cmMap[iski].mergedLG[it]; 
+      }
+      break;
     }
     for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
       sampleHG.push_back(rawhit.highGainADC(it)-subHG[it]);
       sampleLG.push_back(rawhit.lowGainADC(it)-subLG[it]);
+      if( highGain<sampleHG[it] && it>1 && it<5 ) highGain=sampleHG[it];
+      if( lowGain<sampleLG[it] && it>1 && it<5 ) lowGain=sampleLG[it];
       sampleT.push_back(25*it+12.5);
     }
     //this is a just try to isolate hits with signal
-    float en1=rawhit.highGainADC(1)-subHG[1];
-    float en3=rawhit.highGainADC(3)-subHG[3];
-    float en4=rawhit.highGainADC(4)-subHG[4];
-    float en6=rawhit.highGainADC(6)-subHG[6];
-    
-    if( en1<en3 && en3>en6 && en4>en6 && en3>m_timeSample3ADCCut){
-      PulseFitter fitter(0,150);
-      PulseFitterResult fithg;
-      fitter.run(sampleT, sampleHG, fithg);
-      PulseFitterResult fitlg;
-      fitter.run(sampleT, sampleLG, fitlg);
-      
-      highGain = fithg.amplitude;
-      lowGain = fitlg.amplitude;
-      timeHG = fithg.tmax - fithg.trise;
-      timeLG = fitlg.tmax - fitlg.trise;
-      hgStatus = fithg.status;
-      lgStatus = fitlg.status;
-    //  std::cout << rawhit.detid().layer()-1 << " " 
-		//<< rawhit.detid().iu() << " " 
-		//<< rawhit.detid().iv() << " "
-		//<< highGain << " " << hgStatus << " "
-		//<< lowGain << " " << lgStatus << std::endl;
-    }
-    if(hgStatus != 0)
-      highGain=0;
-    if(lgStatus != 0)
-      lowGain=0;
-    
-    float energy = -1;
-    float time = -1.;
+    float en1=sampleHG[1];
+    float en2=sampleHG[2];
+    float en3=sampleHG[3];
+    float en4=sampleHG[4];
+    float en6=sampleHG[6];
     HGCalTBRecHit recHit(rawhit.detid(), energy, lowGain, highGain, totGain, time);
-    if(rawhit.highGainADC(3) < m_highGainADCSaturation && hgStatus == 0){
-      energy = highGain;
-      time = timeHG;
-      recHit.setFlag(HGCalTBRecHit::kGood);
-    }     
-    else if(rawhit.lowGainADC(3)-subHG[3] < m_lowGainADCSaturation && lgStatus == 0){
-      energy = lowGain * m_LG2HG_value.at(iboard);
-      time = timeLG;
-      recHit.setFlag(HGCalTBRecHit::kHighGainSaturated);
-      recHit.setFlag(HGCalTBRecHit::kGood);
-    }
-    else if(totGain > 50){
-      energy = totGain * m_TOT2LG_value.at(iboard) * m_LG2HG_value.at(iboard);
-      recHit.setFlag(HGCalTBRecHit::kLowGainSaturated);
-      recHit.setFlag(HGCalTBRecHit::kGood);
-    }
-    else {
-      energy = 0;
-    }
     if( rawhit.isUnderSaturationForHighGain() ) recHit.setUnderSaturationForHighGain();
     if( rawhit.isUnderSaturationForLowGain() ) recHit.setUnderSaturationForLowGain();
-
-    recHit.setEnergy(energy);
-    recHit.setTime(time);
-
     HGCalTBDetId detid = rawhit.detid();
     CellCentreXY = TheCell.GetCellCentreCoordinatesForPlots(detid.layer(), detid.sensorIU(), detid.sensorIV(), detid.iu(), detid.iv(), SENSORSIZE );
     double iux = (CellCentreXY.first < 0 ) ? (CellCentreXY.first + HGCAL_TB_GEOMETRY::DELTA) : (CellCentreXY.first - HGCAL_TB_GEOMETRY::DELTA);
     double iuy = (CellCentreXY.second < 0 ) ? (CellCentreXY.second + HGCAL_TB_GEOMETRY::DELTA) : (CellCentreXY.second - HGCAL_TB_GEOMETRY::DELTA);
     recHit.setCellCenterCoordinate(iux, iuy);
 
+    if( en1<en3 && en3>en6 && (en4>en6||en2>en6) && en3>m_timeSample3ADCCut){
+
+      HGCalTBLayer layer= essource_.layout_.at(rawhit.detid().layer()-1);
+      int moduleId= layer.at( recHit.id().sensorIU(),recHit.id().sensorIV() ).moduleID();
+      iski = rawhit.skiroc()%4;
+      ASIC_ADC_Conversions adcConv=essource_.adccalibmap_.getASICConversions(moduleId,iski);
+      
+      if( rawhit.lowGainADC(3) > adcConv.TOT_lowGain_transition() ){
+	energy = totGain * adcConv.TOT_to_lowGain() * adcConv.lowGain_to_highGain();
+	recHit.setFlag(HGCalTBRecHit::kLowGainSaturated);
+	recHit.setFlag(HGCalTBRecHit::kGood);
+      }
+      else if( rawhit.highGainADC(3) > adcConv.lowGain_highGain_transition() ){
+	fitter.run(sampleT, sampleLG, fitresult);
+	recHit.setFlag(HGCalTBRecHit::kHighGainSaturated);
+	if( fitresult.status==0 ){
+	  energy = fitresult.amplitude * adcConv.lowGain_to_highGain();
+	  time = fitresult.tmax - fitresult.trise;
+	  recHit.setFlag(HGCalTBRecHit::kGood);
+	}
+      }
+      else{
+	fitter.run(sampleT, sampleHG, fitresult);
+	if( fitresult.status==0 ){
+	  energy = fitresult.amplitude;
+	  time = fitresult.tmax - fitresult.trise;
+	  recHit.setFlag(HGCalTBRecHit::kGood);
+	}
+      }
+      recHit.setEnergy(energy*adcConv.adc_to_MIP());
+      recHit.setTime(time);
+    }
     rechits->push_back(recHit);
   }
   event.put(rechits, m_outputCollectionName);
