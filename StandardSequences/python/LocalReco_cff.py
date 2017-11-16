311861e7c120bc528ace0b2900a042c68775c828
thorben quast <thorben.quast@cern.ch>
2017-11-16 18:50:16 +0100

    [examples] add README, exemplary analyzers using DWCs, configuration files

---
+++ StandardSequences/python/LocalReco_cff.py
@@ -0,0 +1,7 @@
+import FWCore.ParameterSet.Config as cms
+
+from HGCal.Reco.millepede_binarywriter_cfi import *
+from HGCal.Reco.dwc_ntupelizer_cfi import *
+from HGCal.Reco.hgcaltbclusterproducer_cfi import *
+
+
