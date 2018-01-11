f5139e4c71e1906071f383675e0382cf63aaa422
asteencern <arnaud.steen@cern.ch>
2018-01-11 14:41:15 +0100

    remove useless maps and condition files

--- unpack2017_cfg.py
+++ unpack2017_cfg.py
@@ -5,20 +5,15 @@ import os,sys
 
 options = VarParsing.VarParsing('standard') # avoid the options: maxEvents, files, secondaryFiles, output, secondaryOutput because they are already defined in 'standard'
 #Change the data folder appropriately to where you wish to access the files from:
-options.register('dataFolder',
-                 '/disk2_2TB/July2017_TB_data_orm',
-                 VarParsing.VarParsing.multiplicity.singleton,
-                 VarParsing.VarParsing.varType.string,
-                 'folder containing raw input')
 
 options.register('fileName',
-                 'Module63.raw',
+                 './rawdata.raw',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
-                 'Input file to process')
+                 'Input file (path needed) to process')
 
 options.register('outputFolder',
-                 '/afs/cern.ch/work/a/asteen/public/data/july2017/',
+                 './',
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  'Output folder where analysis output are stored')
@@ -30,7 +25,7 @@ options.register('compressedData',
                  'Option to set if the data have beem compressed')
 
 options.register('electronicMap',
-                 "HGCal/CondObjects/data/map_CERN_Hexaboard_September_17Sensors_7EELayers_10FHLayers_V1.txt",
+                 "HGCal/CondObjects/data/map_CERN_Hexaboard_OneModule.txt",
                  VarParsing.VarParsing.multiplicity.singleton,
                  VarParsing.VarParsing.varType.string,
                  'path to the electronic map')
@@ -41,8 +36,6 @@ options.output = "cmsswEvents.root"
 
 options.parseArguments()
 print options
-if not os.path.isdir(options.dataFolder):
-    sys.exit("Error: Data folder not found or inaccessible!")
 
 pedestalHighGain=options.outputFolder+"/pedestalHG.txt"
 pedestalLowGain=options.outputFolder+"/pedestalLG.txt"
@@ -63,7 +56,7 @@ if options.compressedData==1:
     numberOfBytesPerReadOut=15392
 process.source = cms.Source("HGCalTBRawDataSource",
                             ElectronicMap=cms.untracked.string(options.electronicMap),
-                            fileNames=cms.untracked.vstring("file:%s/%s"%(options.dataFolder,options.fileName)),
+                            fileNames=cms.untracked.vstring("file:%s"%(options.fileName)),
                             OutputCollectionName=cms.untracked.string("skiroc2cmsdata"),
                             NumberOfBytesPerReadOut=cms.untracked.uint32(numberOfBytesPerReadOut),
                             NumberOfBytesForTheHeader=cms.untracked.uint32(numberOfBytesForTheHeader),
