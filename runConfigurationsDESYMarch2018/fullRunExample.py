8958e55674ce33214d0d894b94a8b3d132e71567
thorben quast <thorben.quast@cern.ch>
2018-05-18 18:27:48 +0200

    [analysis] extend fullRunExample

--- runConfigurationsDESYMarch2018/fullRunExample.py
+++ runConfigurationsDESYMarch2018/fullRunExample.py
@@ -5,7 +5,7 @@ import os,sys
 
 #full run configuration for DESY (March 2018) run 1174
 #author: Thorben Quast, thorben.quast@cern.ch
-#18 April 2018
+#18 May 2018
 
 #execution on any machine with lxplus 
 #1. source /cvmfs/cms.cern.ch/cmsset_default.sh;
@@ -13,19 +13,22 @@ import os,sys
 #3. cmsenv
 #4. cmsRun runConfigurationsDESYMarch2018/fullRunExample.py
 
-############################hard coded configuration##########################
-
-dataFolder = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/ORM_raw"
+############################hard coded configuration: adjust for each run##########################
 runNumber = 1174
 beamEnergy = 6.0
+setupConfiguration = 6
+SkipFirstNEventsInTelescopeFile = 1
+
+############################hard coded configuration: fixed for all runs documented in the according Google spreadsheet from 28th March 2018#########################
+ORM_dataFolder = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/ORM_raw"
+telescope_dataFolder = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/beamTelescopeReco_corryvreckan/v1"
 beamParticlePDGID = 11
 runType = "Beam"
-setupConfiguration = 6
 
 pedestalHighGainFile = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/quickAnalysis_thorben/pedestals/pedestalsHG_1174.txt"
 pedestalLowGainFile = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/quickAnalysis_thorben/pedestals/pedestalsLG_1174.txt"
 noisyChannelsFile = "/eos/cms/store/group/dpg_hgcal/tb_hgcal/desy_march2018/quickAnalysis_thorben/pedestals/noisyChannels_1174.txt"
-VariablesToPlot = ["xmean", "ymean", "NRechits", "E7_tot", "E19_tot", "EAll_tot", "EAll_HG_tot", "EAll_LG_tot", "EAll_TOT_tot", "EAll_layer1", "EAll_layer2", "EAll_layer3" ]
+VariablesToPlot = ["xmean", "ymean", "NRechits", "E7_tot", "E19_tot", "EAll_tot", "EAllHG_tot", "EAllLG_tot", "EAllTOT_tot", "EAll_layer1", "EAll_layer2", "EAll_layer3" ]
 eventNumbersToDisplay = range(1, 11)        #displays for the first ten events
 outputFile = "output_%s.root" % runNumber
 
@@ -34,7 +37,7 @@ NHexaBoards = 3
 electronicMap = "HGCal/CondObjects/data/map_CERN_Hexaboard_DESY_3Sensors_3EELayers_V1.txt"
 hgcalLayout = "HGCal/CondObjects/data/layerGeom_desymarch2018_configuration4.txt"
 adcCalibrations = "HGCal/CondObjects/data/hgcal_calibration_DESYMarch2018.txt"
-layerPositionFile = "HGCal/CondObjects/data/layer_distances_DESY_March2018_config4.txt"
+layerPositions = "/afs/cern.ch/user/t/tquast/CMSSW_9_3_0/src/HGCal/CondObjects/data/layer_distances_DESY_March2018_config4.txt"     #needs to be the absolute path
 
 
 reportEvery = 1000
@@ -59,7 +62,7 @@ process.TFileService = cms.Service("TFileService", fileName = cms.string(outputF
 
 process.source = cms.Source("HGCalTBRawDataSource",
                             ElectronicMap=cms.untracked.string(electronicMap),
-                            fileNames=cms.untracked.vstring("file:%s/HexaData_Run%04d.raw"%(dataFolder, runNumber)),
+                            fileNames=cms.untracked.vstring("file:%s/HexaData_Run%04d.raw"%(ORM_dataFolder, runNumber)),
                             OutputCollectionName=cms.untracked.string("skiroc2cmsdata"),
                             NumberOf32BitsWordsPerReadOut=cms.untracked.uint32(30787),
                             NumberOfBytesForTheHeader=cms.untracked.uint32(12),
@@ -123,8 +126,17 @@ process.rechitplotter = cms.EDAnalyzer("RecHitPlotter",
                                        NoiseThreshold=cms.untracked.double(20)
 )
 
+
+process.daturaproducer = cms.EDProducer("HGCalTBDATURATelescopeProducer",
+                            OutputCollectionName = cms.string("HGCalTBDATURATracks"), 
+                            RUNDATA = cms.InputTag("source","RunData"),
+                            inputFile = cms.string("%s/reco_%06d.root"%(telescope_dataFolder, runNumber)),
+                            SkipFirstNEventsInTelescopeFile = cms.int32(SkipFirstNEventsInTelescopeFile),  #first event in the beam telescope file is a BORE event
+                            layerPositionFile = cms.string(layerPositions)
+)
+
 process.cellenergyplotting = cms.EDAnalyzer("CellEnergyPlotter",
-                                RUNDATA = cms.InputTag("source", "RunData" ), 
+                                RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ), 
                                 HGCALTBRECHITS = cms.InputTag("rechitproducer","HGCALTBRECHITS" ),
                                 HGCALTBCOMMONMODENOISE = cms.InputTag("rechitproducer","HGCALTBCOMMONMODENOISEMAP" ),
                                 ElectronicMap = cms.untracked.string(electronicMap),
@@ -133,44 +145,68 @@ process.cellenergyplotting = cms.EDAnalyzer("CellEnergyPlotter",
                                 commonModeNoiseRejectionType = cms.int32(0)       #0: none, else 1-..., default: 0 
 )
 
+process.correlationanalysis = cms.EDAnalyzer("DATURATelescopeCorrelator",
+                                RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ), 
+                                DATURATelescopeData = cms.InputTag("daturaproducer","HGCalTBDATURATracks" ), 
+                                HGCALTBRECHITS = cms.InputTag("rechitproducer","HGCALTBRECHITS" ),
+                                ElectronicMap = cms.untracked.string(electronicMap),
+                                DetectorLayout=cms.untracked.string(hgcalLayout),
+                                NHexaBoards=cms.untracked.int32(NHexaBoards),
+                                n_bins = cms.int32(50),
+                                max_dim_x_DUT = cms.double(10.),
+                                max_dim_y_DUT = cms.double(10.)
+)
+
 process.variablecomputation = cms.EDProducer("VariableComputation",
-                                RUNDATA = cms.InputTag("source", "RunData" ), 
+                                RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ), 
                                 MWCHAMBERS = cms.InputTag("wirechamberproducer","DelayWireChambers" ), 
                                 DWCTRACKS = cms.InputTag("dwctrackproducer","HGCalTBDWCTracks" ), 
                                 HGCALTBRECHITS = cms.InputTag("rechitproducer","HGCALTBRECHITS" ),
                                 UserRecordCollectionName=cms.untracked.string("VariableUserRecords"),
                                 ElectronicMap = cms.untracked.string(electronicMap),
                                 DetectorLayout=cms.untracked.string(hgcalLayout),
-                                layerPositionFile=cms.string(layerPositionFile),
+                                layerPositionFile=cms.string(layerPositions),
                                 NHexaBoards=cms.untracked.int32(NHexaBoards),
                                 NLayers=cms.untracked.int32(NHexaBoards),
                                 NColorsInputImage = cms.untracked.int32(-1),
                                 CellEnergyCut = cms.untracked.double(0.5)
 )
 
-process.ntupelizer = cms.EDAnalyzer("NTupelizer",
+process.variablewriter = cms.EDAnalyzer("NTupelizer",
                                 USERRECORDS = cms.InputTag("variablecomputation","VariableUserRecords" ),
                                 UserRecordKeys = cms.vstring(VariablesToPlot)
 )
 
 process.eventdisplay = cms.EDAnalyzer("EventDisplay",
-                                RUNDATA = cms.InputTag("source", "RunData" ), 
+                                RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ), 
                                 HGCALTBRECHITS = cms.InputTag("rechitproducer","HGCALTBRECHITS" ),
-                                MWCHAMBERS = cms.InputTag("wirechamberproducer","DelayWireChambers" ), 
-                                DWCTRACKS = cms.InputTag("dwctrackproducer","HGCalTBDWCTracks" ), 
+                                MWCHAMBERS = cms.InputTag("","" ), 
+                                DWCTRACKS = cms.InputTag("","" ), 
                                 electronicsMap = cms.untracked.string(electronicMap),
                                 NHexaBoards=cms.untracked.int32(NHexaBoards),
                                 eventsToPlot=cms.vint32(eventNumbersToDisplay)
                               )
 
+process.rechitntupler = cms.EDAnalyzer("RecHitNtupler",
+                                       InputCollection=cms.InputTag("rechitproducer","HGCALTBRECHITS"),
+                                       RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ),
+                                       ElectronicMap=cms.untracked.string(electronicMap),
+                                       DetectorLayout=cms.untracked.string(hgcalLayout),
+                                       SensorSize=cms.untracked.int32(128),
+                                       EventPlotter=cms.untracked.bool(True),
+                                       MipThreshold=cms.untracked.double(5.0),
+                                       NoiseThreshold=cms.untracked.double(0.5)
+)
 
+process.trackimpactntupler = cms.EDAnalyzer("ImpactPointNtupler",
+                                       DATURATelescopeData = cms.InputTag("daturaproducer","HGCalTBDATURATracks" ),
+                                       RUNDATA = cms.InputTag("daturaproducer", "FullRunData" ),
+                                       nLayers=cms.untracked.int32(NHexaBoards)
+)
 
 ####################################
-# Load the standard sequences
-process.load('HGCal.StandardSequences.LocalReco_cff')
-process.load('HGCal.StandardSequences.RawToDigi_cff')
-####################################
 
 
-process.p = cms.Path(process.rawhitproducer*process.rawhitplotter*process.rechitproducer*process.rechitplotter*process.cellenergyplotting*process.variablecomputation*process.ntupelizer*process.eventdisplay)
+process.p = cms.Path(process.rawhitproducer*process.rawhitplotter*process.rechitproducer*process.rechitplotter*process.daturaproducer*process.cellenergyplotting*process.correlationanalysis*process.variablecomputation*process.variablewriter*process.eventdisplay*process.rechitntupler*process.trackimpactntupler)
+
 
