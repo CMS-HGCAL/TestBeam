eb6c2fb223466416991ef004d2ced1a580c2488d
asteencern <arnaud.steen@cern.ch>
2018-06-25 11:06:31 +0200

    [merge] 5827ed9f3d10c9abb96ea836afcf15f8fcbe6aad

--- Reco/src/CommonMode.cc
+++ Reco/src/CommonMode.cc
@@ -1,5 +1,6 @@
 #include <HGCal/Reco/interface/CommonMode.h>
 #include <HGCal/Geometry/interface/HGCalTBTopology.h>
+#include <HGCal/Geometry/interface/HGCalTBGeometryParameters.h>
 
 CommonMode::CommonMode( HGCalElectronicsMap &emap, bool useMedian, bool cmPerChip, float threshold ) : _useMedian(useMedian),
 												      _cmPerChip(cmPerChip),
@@ -23,223 +24,148 @@ void CommonMode::Evaluate( edm::Handle<HGCalTBRawHitCollection> hits )
 
 void CommonMode::EvaluateMedianPerChip( edm::Handle<HGCalTBRawHitCollection> hits )
 {
-  std::map< int,std::vector<float> > cmMapHG;
-  std::map< int,std::vector<float> > cmMapLG;
-  std::map< int,std::vector<float> > cmMapHGHalf;
-  std::map< int,std::vector<float> > cmMapLGHalf;
+  std::vector<float> cmMapHG[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapLG[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapHGHalf[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapLGHalf[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA*NUMBER_OF_TIME_SAMPLES];
+  for( int iski=0; iski<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA; iski++ ){
+    for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      cmMapHG[NUMBER_OF_TIME_SAMPLES*iski + it].reserve(32);
+      cmMapLG[NUMBER_OF_TIME_SAMPLES*iski + it].reserve(32);
+      cmMapHGHalf[NUMBER_OF_TIME_SAMPLES*iski + it].reserve(32);
+      cmMapLGHalf[NUMBER_OF_TIME_SAMPLES*iski + it].reserve(32);
+    }
+  }
   for( auto hit : *hits ){
     HGCalTBElectronicsId eid( _emap.detId2eid(hit.detid().rawId()) );
     if( !_emap.existsEId(eid) ) continue;
     int iski=hit.skiroc();
-    if( hit.detid().cellType()==0 || hit.detid().cellType()==4 ) {
-      for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-	
-  if (thresholdHG==-1 || hit.highGainADC(it)<thresholdHG) {
-    if( cmMapHG.find(100*iski+it)==cmMapHG.end() ){
-      std::vector<float> vec; vec.push_back( hit.highGainADC(it) );
-      std::pair< int,std::vector<float> > p(100*iski+it,vec);
-      cmMapHG.insert(p);
+    for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      if( hit.detid().cellType()==0 || hit.detid().cellType()==4 ) {
+	     cmMapHG[NUMBER_OF_TIME_SAMPLES*iski+it].push_back(hit.highGainADC(it));
+	     cmMapLG[NUMBER_OF_TIME_SAMPLES*iski+it].push_back(hit.lowGainADC(it));
+      }
+      else if( hit.detid().cellType()!=5 ){
+	   cmMapHGHalf[NUMBER_OF_TIME_SAMPLES*iski+it].push_back(hit.highGainADC(it));
+	   cmMapLGHalf[NUMBER_OF_TIME_SAMPLES*iski+it].push_back(hit.lowGainADC(it));
+      }
     }
-    else cmMapHG[100*iski+it].push_back( hit.highGainADC(it) );
   }
 
-  if (thresholdLG==-1 || hit.lowGainADC(it)<thresholdLG) {
-  	if( cmMapLG.find(100*iski+it)==cmMapLG.end() ){
-  	  std::vector<float> vec; vec.push_back( hit.lowGainADC(it) );
-  	  std::pair< int,std::vector<float> > p(100*iski+it,vec);
-  	  cmMapLG.insert(p);
-  	}
-	 else cmMapLG[100*iski+it].push_back( hit.lowGainADC(it) );
-  }
-      }
-    }
-    else if( hit.detid().cellType()!=5 ){
-      for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-    	if( cmMapHGHalf.find(100*iski+it)==cmMapHGHalf.end() ){
-	  std::vector<float> vec; vec.push_back( hit.highGainADC(it) );
-	  std::pair< int,std::vector<float> > p(100*iski+it,vec);
-    	  cmMapHGHalf.insert(p);
-	}
-	else cmMapHGHalf[100*iski+it].push_back( hit.highGainADC(it) );
-    	if( cmMapLGHalf.find(100*iski+it)==cmMapLGHalf.end() ){
-	  std::vector<float> vec; vec.push_back( hit.lowGainADC(it) );
-	  std::pair< int,std::vector<float> > p(100*iski+it,vec);
-    	  cmMapLGHalf.insert(p);
-	}
-	else cmMapLGHalf[100*iski+it].push_back( hit.highGainADC(it) );
-      }
-    }
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapHG.begin(); it!=cmMapHG.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t iski=it->first/100;
-    if( _cmMap.find( iski )==_cmMap.end() ){
-      commonModeNoise cm;
-      cm.fullHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      cm.outerHG[ts] = cm.fullHG[ts];
-      cm.mergedHG[ts] = cm.fullHG[ts]*1.5;
-      std::pair<int,commonModeNoise> p(iski,cm);
-      _cmMap.insert(p);
-    }
-    else{
-      _cmMap[iski].fullHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      _cmMap[iski].outerHG[ts] = _cmMap[iski].fullHG[ts];
-      _cmMap[iski].mergedHG[ts] = _cmMap[iski].fullHG[ts]*1.5;
-    }
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapLG.begin(); it!=cmMapLG.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t iski=it->first/100;
-    if( _cmMap.find( iski )==_cmMap.end() ){
-      commonModeNoise cm;
-      cm.fullLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      cm.outerLG[ts] = cm.fullLG[ts];
-      cm.mergedLG[ts] = cm.fullLG[ts]*1.5;
-      std::pair<int,commonModeNoise> p(iski,cm);
-      _cmMap.insert(p);
-    }
-    _cmMap[iski].fullLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-    _cmMap[iski].outerLG[ts] = _cmMap[iski].fullLG[ts];
-    _cmMap[iski].mergedLG[ts] = _cmMap[iski].fullLG[ts]*1.5;
-  }
   HGCalTBTopology topo;
-  for( std::map< int,std::vector<float> >::iterator it=cmMapHGHalf.begin(); it!=cmMapHGHalf.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t iski=it->first/100;
-    if( _cmMap.find( iski )==_cmMap.end() ){
-      commonModeNoise cm;
-      cm.halfHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      cm.mouseBiteHG[ts] = cm.halfHG[ts]*topo.Cell_Area(3)/topo.Cell_Area(2);
-      std::pair<int,commonModeNoise> p(iski,cm);
-      _cmMap.insert(p);
+  for( int iski=0; iski<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*HGCAL_TB_GEOMETRY::N_SKIROC_PER_HEXA; iski++ ){
+    for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      int index=NUMBER_OF_TIME_SAMPLES*iski+it;
+      std::sort( cmMapHG[index].begin(),cmMapHG[index].end() );
+      std::sort( cmMapLG[index].begin(),cmMapLG[index].end() );
+      std::sort( cmMapHGHalf[index].begin(),cmMapHGHalf[index].end() );
+      std::sort( cmMapLGHalf[index].begin(),cmMapLGHalf[index].end() );
+      if( _cmMap.find( iski )==_cmMap.end() ){
+	commonModeNoise cm;
+	uint16_t size = cmMapHG[index].size();
+	cm.fullHG[it] = size%2==0 ? cmMapHG[index][size/2-1] : cmMapHG[index][size/2] ;
+	cm.outerHG[it] = cm.fullHG[it];
+	cm.mergedHG[it] = cm.fullHG[it]*1.5;
+	cm.fullLG[it] = size%2==0 ? cmMapLG[index][size/2-1] : cmMapLG[index][size/2] ;
+	cm.outerLG[it] = cm.fullLG[it];
+	cm.mergedLG[it] = cm.fullLG[it]*1.5;
+
+	size = cmMapHGHalf[index].size();
+	cm.halfHG[it] = size%2==0 ? cmMapHGHalf[index][size/2-1] : cmMapHGHalf[index][size/2] ;
+	cm.mouseBiteHG[it] = cm.halfHG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	cm.halfLG[it] = size%2==0 ? cmMapLGHalf[index][size/2-1] : cmMapLGHalf[index][size/2] ;
+	cm.mouseBiteLG[it] = cm.halfLG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	std::pair<int,commonModeNoise> p(iski,cm);
+	_cmMap.insert(p);
+      }
+      else{
+	uint16_t size = cmMapHG[index].size();
+	_cmMap[iski].fullHG[it] = size%2==0 ? cmMapHG[index][size/2-1] : cmMapHG[index][size/2] ;
+	_cmMap[iski].outerHG[it] = _cmMap[iski].fullHG[it];
+	_cmMap[iski].mergedHG[it] = _cmMap[iski].fullHG[it]*1.5;
+	_cmMap[iski].fullLG[it] = size%2==0 ? cmMapLG[index][size/2-1] : cmMapLG[index][size/2] ;
+	_cmMap[iski].outerLG[it] = _cmMap[iski].fullLG[it];
+	_cmMap[iski].mergedLG[it] = _cmMap[iski].fullLG[it]*1.5;
+	size = cmMapHGHalf[index].size();
+	_cmMap[iski].halfHG[it] = size%2==0 ? cmMapHGHalf[index][size/2-1] : cmMapHGHalf[index][size/2] ;
+	_cmMap[iski].mouseBiteHG[it] = _cmMap[iski].halfHG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	_cmMap[iski].halfLG[it] = size%2==0 ? cmMapLGHalf[index][size/2-1] : cmMapLGHalf[index][size/2] ;
+	_cmMap[iski].mouseBiteLG[it] = _cmMap[iski].halfLG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+      }
     }
-    _cmMap[iski].halfHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-    _cmMap[iski].mouseBiteHG[ts] = _cmMap[iski].halfHG[ts]*topo.Cell_Area(3)/topo.Cell_Area(2);
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapLGHalf.begin(); it!=cmMapLGHalf.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t iski=it->first/100;
-    if( _cmMap.find( iski )==_cmMap.end() ){
-      commonModeNoise cm;
-      cm.halfLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      cm.mouseBiteLG[ts] = cm.halfLG[ts]*topo.Cell_Area(3)/topo.Cell_Area(2);
-      std::pair<int,commonModeNoise> p(iski,cm);
-      _cmMap.insert(p);
-    }
-    _cmMap[iski].halfLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-    _cmMap[iski].mouseBiteLG[ts] = _cmMap[iski].halfLG[ts]*topo.Cell_Area(3)/topo.Cell_Area(2);
   }
 }
 
 void CommonMode::EvaluateMedianPerLayer( edm::Handle<HGCalTBRawHitCollection> hits )
 {
-  std::map< int,std::vector<float> > cmMapHG;
-  std::map< int,std::vector<float> > cmMapLG;
-  std::map< int,std::vector<float> > cmMapHGHalf;
-  std::map< int,std::vector<float> > cmMapLGHalf;
+  std::vector<float> cmMapHG[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapLG[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapHGHalf[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*NUMBER_OF_TIME_SAMPLES];
+  std::vector<float> cmMapLGHalf[HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD*NUMBER_OF_TIME_SAMPLES];
+  for( int ilayer=0; ilayer<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD; ilayer++ ){
+    for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      cmMapHG[NUMBER_OF_TIME_SAMPLES*ilayer + it].reserve(128);
+      cmMapLG[NUMBER_OF_TIME_SAMPLES*ilayer + it].reserve(128);
+      cmMapHGHalf[NUMBER_OF_TIME_SAMPLES*ilayer + it].reserve(128);
+      cmMapLGHalf[NUMBER_OF_TIME_SAMPLES*ilayer + it].reserve(128);
+    }
+  }
   for( auto hit : *hits ){
     HGCalTBElectronicsId eid( _emap.detId2eid(hit.detid().rawId()) );
     if( !_emap.existsEId(eid) ) continue;
-    int ilayer=hit.detid().layer();
-    if( hit.detid().cellType()==0 || hit.detid().cellType()==4 ) {
-      for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-	if( cmMapHG.find(100*ilayer+it)==cmMapHG.end() ){
-	  std::vector<float> vech; vech.push_back( hit.highGainADC(it) );
-	  std::vector<float> vecl; vecl.push_back( hit.lowGainADC(it) );
-	  std::pair< int,std::vector<float> > ph(100*ilayer+it,vech);
-	  std::pair< int,std::vector<float> > pl(100*ilayer+it,vecl);
-	  cmMapHG.insert(ph);
-	  cmMapLG.insert(pl);
-	}
-	else{
-	  cmMapHG[100*ilayer+it].push_back( hit.highGainADC(it) );
-	  cmMapLG[100*ilayer+it].push_back( hit.lowGainADC(it) );
-	}
+    int ilayer=hit.detid().layer()-1;
+    for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      if( hit.detid().cellType()==0 || hit.detid().cellType()==4 ) {
+	cmMapHG[NUMBER_OF_TIME_SAMPLES*ilayer+it].push_back(hit.highGainADC(it));
+	cmMapLG[NUMBER_OF_TIME_SAMPLES*ilayer+it].push_back(hit.lowGainADC(it));
       }
-    }
-    else if( hit.detid().cellType()!=5 ){
-      for( size_t it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
-	if( cmMapHGHalf.find(100*ilayer+it)==cmMapHGHalf.end() ){
-	  std::vector<float> vech; vech.push_back( hit.highGainADC(it) );
-	  std::vector<float> vecl; vecl.push_back( hit.lowGainADC(it) );
-	  std::pair< int,std::vector<float> > ph(100*ilayer+it,vech);
-	  std::pair< int,std::vector<float> > pl(100*ilayer+it,vecl);
-	  cmMapHGHalf.insert(ph);
-	  cmMapLGHalf.insert(pl);
-	}
-	else{
-	  cmMapHGHalf[100*ilayer+it].push_back( hit.highGainADC(it) );
-	  cmMapLGHalf[100*ilayer+it].push_back( hit.lowGainADC(it) );
-	}
+      else if( hit.detid().cellType()!=5 ){
+	cmMapHGHalf[NUMBER_OF_TIME_SAMPLES*ilayer+it].push_back(hit.highGainADC(it));
+	cmMapLGHalf[NUMBER_OF_TIME_SAMPLES*ilayer+it].push_back(hit.lowGainADC(it));
       }
     }
   }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapHG.begin(); it!=cmMapHG.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t ilayer=it->first/100;
-    if( _cmMap.find( ilayer )==_cmMap.end() ){
-      commonModeNoise cm;
-      cm.fullHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      cm.outerHG[ts] = cm.fullHG[ts];
-      cm.mergedHG[ts] = cm.fullHG[ts]*1.5;
-      std::pair<int,commonModeNoise> p(ilayer,cm);
-      _cmMap.insert(p);
-    }
-    else{
-      _cmMap[ilayer].fullHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      _cmMap[ilayer].outerHG[ts] = _cmMap[ilayer].fullHG[ts];
-      _cmMap[ilayer].mergedHG[ts] = _cmMap[ilayer].fullHG[ts]*1.5;
-    }
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapLG.begin(); it!=cmMapLG.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t ilayer=it->first/100;
-    if( _cmMap.find( ilayer )==_cmMap.end() ){
-      std::cout << "the _cmMap should already contain element with key (layerId) = " << ilayer << " -> exit(1) " << std::endl;
-      exit(1);
-    }
-    _cmMap[ilayer].fullLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-    _cmMap[ilayer].outerLG[ts] = _cmMap[ilayer].fullLG[ts];
-    _cmMap[ilayer].mergedLG[ts] = _cmMap[ilayer].fullLG[ts]*1.5;
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapHGHalf.begin(); it!=cmMapHGHalf.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t ilayer=it->first/100;
-    if( _cmMap.find( ilayer )==_cmMap.end() ){
-      std::cout << "the _cmMap should already contain element with key (layerId) = " << ilayer << " -> exit(1) " << std::endl;
-      exit(1);
-    }
-    else{
-      _cmMap[ilayer].mouseBiteHG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      _cmMap[ilayer].halfHG[ts] = _cmMap[ilayer].mouseBiteHG[ts];
-    }
-  }
-  for( std::map< int,std::vector<float> >::iterator it=cmMapLGHalf.begin(); it!=cmMapLGHalf.end(); ++it ){
-    std::sort( it->second.begin(),it->second.end() );
-    uint16_t size = it->second.size();
-    uint16_t ts=it->first%100;
-    uint16_t ilayer=it->first/100;
-    if( _cmMap.find( ilayer )==_cmMap.end() ){
-      std::cout << "the _cmMap should already contain element with key (layerId) = " << ilayer << " -> exit(1) " << std::endl;
-      exit(1);
-    }
-    else{
-      _cmMap[ilayer].mouseBiteLG[ts] = size%2==0 ? it->second.at(size/2-1) : it->second.at(size/2) ;
-      _cmMap[ilayer].halfLG[ts] = _cmMap[ilayer].mouseBiteLG[ts];
+
+  HGCalTBTopology topo;
+  for( int ilayer=0; ilayer<HGCAL_TB_GEOMETRY::NUMBER_OF_HEXABOARD; ilayer++ ){
+    for( int it=0; it<NUMBER_OF_TIME_SAMPLES; it++ ){
+      int index=NUMBER_OF_TIME_SAMPLES*ilayer+it;
+      std::sort( cmMapHG[index].begin(),cmMapHG[index].end() );
+      std::sort( cmMapLG[index].begin(),cmMapLG[index].end() );
+      std::sort( cmMapHGHalf[index].begin(),cmMapHGHalf[index].end() );
+      std::sort( cmMapLGHalf[index].begin(),cmMapLGHalf[index].end() );
+      if( _cmMap.find( ilayer )==_cmMap.end() ){
+	commonModeNoise cm;
+	uint16_t size = cmMapHG[index].size();
+	cm.fullHG[it] = size%2==0 ? cmMapHG[index][size/2-1] : cmMapHG[index][size/2] ;
+	cm.outerHG[it] = cm.fullHG[it];
+	cm.mergedHG[it] = cm.fullHG[it]*1.5;
+	cm.fullLG[it] = size%2==0 ? cmMapLG[index][size/2-1] : cmMapLG[index][size/2] ;
+	cm.outerLG[it] = cm.fullLG[it];
+	cm.mergedLG[it] = cm.fullLG[it]*1.5;
+
+	size = cmMapHGHalf[index].size();
+	cm.halfHG[it] = size%2==0 ? cmMapHGHalf[index][size/2-1] : cmMapHGHalf[index][size/2] ;
+	cm.mouseBiteHG[it] = cm.halfHG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	cm.halfLG[it] = size%2==0 ? cmMapLGHalf[index][size/2-1] : cmMapLGHalf[index][size/2] ;
+	cm.mouseBiteLG[it] = cm.halfLG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	std::pair<int,commonModeNoise> p(ilayer,cm);
+	_cmMap.insert(p);
+      }
+      else{
+	uint16_t size = cmMapHG[index].size();
+	_cmMap[ilayer].fullHG[it] = size%2==0 ? cmMapHG[index][size/2-1] : cmMapHG[index][size/2] ;
+	_cmMap[ilayer].outerHG[it] = _cmMap[ilayer].fullHG[it];
+	_cmMap[ilayer].mergedHG[it] = _cmMap[ilayer].fullHG[it]*1.5;
+	_cmMap[ilayer].fullLG[it] = size%2==0 ? cmMapLG[index][size/2-1] : cmMapLG[index][size/2] ;
+	_cmMap[ilayer].outerLG[it] = _cmMap[ilayer].fullLG[it];
+	_cmMap[ilayer].mergedLG[it] = _cmMap[ilayer].fullLG[it]*1.5;
+	size = cmMapHGHalf[index].size();
+	_cmMap[ilayer].halfHG[it] = size%2==0 ? cmMapHGHalf[index][size/2-1] : cmMapHGHalf[index][size/2] ;
+	_cmMap[ilayer].mouseBiteHG[it] = _cmMap[ilayer].halfHG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+	_cmMap[ilayer].halfLG[it] = size%2==0 ? cmMapLGHalf[index][size/2-1] : cmMapLGHalf[index][size/2] ;
+	_cmMap[ilayer].mouseBiteLG[it] = _cmMap[ilayer].halfLG[it]*topo.Cell_Area(3)/topo.Cell_Area(2);
+      }
     }
   }
 }
