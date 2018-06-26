a3a7a5e3220f6b65dc1f3ef8ac03d28b9e2d943a
thorben quast <thorben.quast@cern.ch>
2018-06-26 14:26:22 +0200

    [dwcs] remove redundancy in DWC synchronisation

--- RawToDigi/plugins/HGCalTBWireChamberSource.cc
+++ RawToDigi/plugins/HGCalTBWireChamberSource.cc
@@ -1,6 +1,6 @@
 #include "HGCal/RawToDigi/plugins/HGCalTBWireChamberSource.h"
 
-//#define DEBUG
+#define DEBUG
 
 
 bool validTimestamp(int ts) {
@@ -353,7 +353,7 @@ void HGCalTBWireChamberSource::produce(edm::Event & event) {
 		double deltaTs = (event_trigger_time[event_candidate_index]-ref_time_sync) - (timeSinceStart_ms - ref_time_dwc);
 		rd->doubleUserRecords.add("triggerDeltaT_to_TDC", deltaTs);
 
-		if (allowForTDCEventSkipping[fileCounter]&&(deltaTs>-100.)&&(deltaTs<-15.)&&(fabs(delta_T_priorDWCTrigger+deltaTs)>2.)) {		
+		if (allowForTDCEventSkipping[fileCounter]&&(deltaTs>-100.)&&(deltaTs<-15.)) {		
 		//average time in between two events is around 20ms given by the sync board. So cutting on -15. is reasonable for this configuration (20 Oct 2017 in H6A)
 			skippedTDCTriggers+=1;
 		}
